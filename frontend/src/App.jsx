b0ee37dbe53e142030e4f5b5fb574056e3a71c7f
Adit2002 <95736811+Adit2002@users.noreply.github.com>
2024-02-23 16:31:08 +0530

    commit in configapp, added marked

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,58 @@
 import "./App.css";
+<<<<<<< Updated upstream
 import { useState } from "react";
 import axios from "axios";
+=======
+import React, { useState } from 'react';
+import axios from 'axios';
+import {marked} from 'marked';
+>>>>>>> Stashed changes
 const App = () => {
   let [value, setValue] = useState("");
   let [data, setData] = useState({});
   let [count, setCount] = useState(0);
   let [loading, setLoading] = useState(false);
+<<<<<<< Updated upstream
   let handlePrompt = async () => {
     setCount((p) => p + 1);
     setLoading((p) => !p);
+=======
+  let [formattedResponse,setFormattedResponse]=useState("");
+
+  const handlePrompt = async () => {
+    setCount((p) => p + 1);
+    setLoading(true);
+
+>>>>>>> Stashed changes
     setData({ user: value, response: "..." });
     let data = await axios.post("http://localhost:3000/chat/bot1", {
       prompt: value,
     });
     setValue("");
+<<<<<<< Updated upstream
     setData({ ...data.data });
     setLoading((p) => !p);
   };
+=======
+    setData({ ...response.data });
+    setLoading(false); // Set loading to false when response is received
+    setFormattedResponse(boldText(response.data.response));
+    console.log(formattedResponse);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handlePrompt();
+    }
+  };
+
+  const boldText = (text) => {
+    const res = marked.parse(text);
+    console.log(res);
+    return res;
+  };
+
+>>>>>>> Stashed changes
   return (
     <div className="app">
       <div className="content">
@@ -85,6 +121,7 @@ const App = () => {
               onChange={(e) => {
                 setValue(e.target.value);
               }}
+              onKeyDown={handleKeyDown}
             />
           )}
           {loading === false ? (
